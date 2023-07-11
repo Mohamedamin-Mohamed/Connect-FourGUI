5eb125dffe250cdf9fac1bba5252bfde31c707d1
Mohamedamin-Mohamed <101759235+Mohamedamin-Mohamed@users.noreply.github.com>
2023-07-11 15:41:01 -0500

    Update CheckWinner.java

--- CheckWinner.java
+++ CheckWinner.java
@@ -24,77 +24,55 @@ public class CheckWinner {
 	}
 	
 	public boolean checkWinner() {
-		Color currColor = buttons[row][col].getBackground(); 
-//background color of current index will be returned
-
-		// Check horizontally
-		for (int j = col - 3; j <= col; j++) {
-			if (j >= 0 && j + 3 < 7) {
-				if 
-(buttons[row][j].getBackground().equals(currColor)
-						&& buttons[row][j + 
-1].getBackground().equals(currColor)
-						&& buttons[row][j + 
-2].getBackground().equals(currColor)
-						&& buttons[row][j + 
-3].getBackground().equals(currColor)) {
-					return true;
-				}
-			}
-		}
-
-		// Check vertically
-		for (int i = row - 3; i <= row; i++) {
-			if (i >= 0 && i + 3 < 6) {
-				if 
-(buttons[i][col].getBackground().equals(currColor)
-						&& buttons[i + 
-1][col].getBackground().equals(currColor)
-						&& buttons[i + 
-2][col].getBackground().equals(currColor)
-						&& buttons[i + 
-3][col].getBackground().equals(currColor)) {
-					return true;
-				}
-			}
-		}
-
-		// Check diagonal (down-right)
-		for (int i = row - 3, j = col - 3; i <= row && j <= col; 
-i++, j++) {
-			if (i >= 0 && i + 3 < 6 && j >= 0 && j + 3 < 7) {
-				if 
-(buttons[i][j].getBackground().equals(currColor)
-						&& buttons[i + 1][j + 
-1].getBackground().equals(currColor)
-						&& buttons[i + 2][j + 
-2].getBackground().equals(currColor)
-						&& buttons[i + 3][j + 
-3].getBackground().equals(currColor)) {
-					return true;
-				}
-			}
-		}
-
-		// Check diagonal (up-right)
-		for (int i = row + 3, j = col - 3; i >= row && j <= col; 
-i--, j++) {
-			if (i >= 0 && i - 3 < 6 && j >= 0 && j + 3 < 7) {
-				if 
-(buttons[i][j].getBackground().equals(currColor)
-						&& buttons[i - 1][j + 
-1].getBackground().equals(currColor)
-						&& buttons[i - 2][j + 
-2].getBackground().equals(currColor)
-						&& buttons[i - 3][j + 
-3].getBackground().equals(currColor)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
+        Color currColor = buttons[row][col].getBackground(); //background color of current index will be returned
+		
+	// Check horizontally
+    for (int j = col - 3; j <= col; j++) {
+	if (j >= 0 && j + 3 < 7) {
+	if (buttons[row][j].getBackground().equals(currColor)
+	&& buttons[row][j + 1].getBackground().equals(currColor)
+	&& buttons[row][j + 2].getBackground().equals(currColor)
+	&& buttons[row][j + 3].getBackground().equals(currColor)) {
+	return true;
+	   }
 	}
+}
+	// Check vertically
+   for (int i = row - 3; i <= row; i++) {
+	if (i >= 0 && i + 3 < 6) {
+	if (buttons[i][col].getBackground().equals(currColor)
+	&& buttons[i + 1][col].getBackground().equals(currColor)
+	&& buttons[i + 2][col].getBackground().equals(currColor)
+	&& buttons[i + 3][col].getBackground().equals(currColor)) {
+	return true;
+	  }
+    }
+}
+	// Check diagonal (down-right)
+   for (int i = row - 3, j = col - 3; i <= row && j <= col; i++, j++) {
+	if (i >= 0 && i + 3 < 6 && j >= 0 && j + 3 < 7) {
+	if (buttons[i][j].getBackground().equals(currColor)
+	&& buttons[i + 1][j + 1].getBackground().equals(currColor)
+	&& buttons[i + 2][j + 2].getBackground().equals(currColor)
+	&& buttons[i + 3][j + 3].getBackground().equals(currColor)) {
+	return true;
+	}
+   }
+}
+	// Check diagonal (up-right)
+  for (int i = row + 3, j = col - 3; i >= row && j <= col; i--, j++) {
+	if (i >= 0 && i - 3 < 6 && j >= 0 && j + 3 < 7) {
+	if buttons[i][j].getBackground().equals(currColor)
+	&& buttons[i - 1][j + 1].getBackground().equals(currColor)
+	&& buttons[i - 2][j + 2].getBackground().equals(currColor)
+	&& buttons[i - 3][j + 3].getBackground().equals(currColor)) {
+	return true;
+		}
+	}
+}
+	return false;
+	}
+	
 	public boolean getWinner() {
 		return winner;
 	}
