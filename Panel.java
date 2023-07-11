d8dd40aa3c5f7d143964193f2cf0b57adbe42e27
Mohamedamin-Mohamed <101759235+Mohamedamin-Mohamed@users.noreply.github.com>
2023-07-11 15:42:09 -0500

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -10,23 +10,19 @@ import javax.swing.JPanel;
  * Modified: 07/11/2023              
  * Description:This class represents the game board panel.
  */
-public class Panel{
+ public class Panel{
 	private JPanel panel;
-	
-	Panel(JButton[][] buttons){
-		//we'll use the panel as out board, which will have 6 rows 
-& 7 columns
-		panel = new JPanel(new GridLayout(6, 7));
-		
-		for(int i = 0; i < buttons.length; i++) {
-			for(int j = 0; j < buttons[i].length;j++) {
-				panel.add(buttons[i][j]);
-			}
-		}
+	  
+	Panel(JButton[][] buttons){ //we'll use the panel as out board, which will have 6 rows & 7 columns
+	panel = new JPanel(new GridLayout(6, 7));	
+   for(int i = 0; i < buttons.length; i++) {
+     for(int j = 0; j < buttons[i].length;j++) {
+	panel.add(buttons[i][j]);
 	}
-	public JPanel getPanel() {
-		return panel; //the board will be returned, example is so 
-as to be added to the frame
+     }
+}
+    public JPanel getPanel() {
+	return panel; //the board will be returned, example is so as to be added to the frame
 	}
 }
 
