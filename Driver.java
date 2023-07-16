5e55d716f3d7c6c8333cb74142b95c38679e2012
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2023-07-16 11:43:48 -0500

    sets up the main application window

--- Driver.java
+++ Driver.java
@@ -1,8 +1,16 @@
 import javax.swing.SwingUtilities;
 
+/* 
+ * Files: Driver.java          
+ * Author: Mohamedamin Mohamed          
+ * Contact mohamedamin204080@gmail.com  
+ * Created 07/10/2023    
+ * modified 07/16/2023               
+ * Description:This class sets up the main application window
+ */
 public class Driver {
-
 	public static void main(String[] args) {
+		
 		SwingUtilities.invokeLater(()->{;
 		new Connect_FourGUI();
 		});
