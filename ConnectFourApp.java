32460eca0e5d4c5ec843e84e73a88cb03bd15c63
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2023-07-16 11:48:39 -0500

    sets up the necessary components and establishes the connections between them to create a functioning App

--- ConnectFourApp.java
+++ ConnectFourApp.java
@@ -1,4 +1,13 @@
-import javax.swing.SwingUtilities;;
+import javax.swing.SwingUtilities;
+
+/*
+ * Files: ConnectFourApp.java          
+ * Author: Mohamedamin Mohamed          
+ * Contact mohamedamin204080@gmail.com  
+ * Created 07/10/2023                   
+ * Modified: 07/16/2023              
+ * Description:This class sets up the necessary components and establishes the connections between them to create a functioning App
+ */
 public class ConnectFourApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
